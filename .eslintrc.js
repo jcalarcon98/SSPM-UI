7d98018d2899bbe12fda20926e47cb493b81a323
Jean Carlos Alarcón <jeancalarcon98@gmail.com>
2021-02-09 14:50:22 -0500

    [ADD]: All packages nedded added

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    'no-param-reassign': 0,
+    'no-restricted-syntax': 0,
+    'no-await-in-loop': 0,
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+  plugins: ['jest'],
+};
